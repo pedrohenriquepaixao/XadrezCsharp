1d432f14bfdee8f93a26dd688e43bc2996fd359b
Pedro Henrique <pedrohenriquedapaixao@outlook.com>
2021-07-10 14:57:59 -0300

    Tratando erros com excecao personalizada

--- xadrex-console/Program.cs
+++ xadrex-console/Program.cs
@@ -8,14 +8,21 @@ namespace xadrex_console
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
-            tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab,Cor.Preta), new Posicao(2, 4));
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-            Tela.imprimirTabuleiro(tab);
+                Tela.imprimirTabuleiro(tab);
 
-            Console.WriteLine();
+                Console.WriteLine();
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
         }
 
     }
