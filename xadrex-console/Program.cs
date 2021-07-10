65e7b5a83fa817c9c0e6a3a3874d8633fe1f3026
Pedro Henrique <pedrohenriquedapaixao@outlook.com>
2021-07-10 13:31:15 -0300

    Criando classe Posicao

--- xadrex-console/Program.cs
+++ xadrex-console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tabuleiro;
 
 namespace xadrex_console
 {
@@ -6,7 +7,12 @@ namespace xadrex_console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+            Console.ReadLine();
         }
     }
 }
